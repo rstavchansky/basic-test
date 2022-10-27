2fb8d264e555910f730315276ce84811a7f7bebf
rstavchansky <rachael@netlify.com>
2022-10-27 17:14:48 -0400

    adding a button to test emails integration

---
+++ js/script.js
@@ -0,0 +1,29 @@
+sendEmailButton = document.querySelector(".emailTest");
+
+var sendEmail = function () {
+
+ import fetch from 'node-fetch'
+
+ await fetch(
+    `${process.env.URL}/.netlify/functions/emails/welcome`,
+    {
+      headers: {
+        "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        from: "no-reply@yourdomain.com",
+        to: "jane.example@test.com",
+        cc: "cc@test.com",
+        bcc: "bcc@test.com",
+        subject: "Welcome",
+        parameters: {
+          products: ["product1", "product2", "product3"],
+          name: "Jane",
+        },
+      }),
+    }
+  );
+}
+
+sendEmailButton.addEventListener("click", sendEmail);
