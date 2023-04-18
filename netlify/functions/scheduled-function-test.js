f9d731fad97d981ace4d48d103a6feab346ee3ad
rstavchansky <rachael@netlify.com>
2023-04-18 11:46:00 -0400

    test scheduled function for screenshot update

---
+++ netlify/functions/scheduled-function-test.js
@@ -0,0 +1,14 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/test-scheduled-function.js
+
+const { schedule } = require("@netlify/functions");
+
+const handler = async function(event, context) {
+    console.log("Received event:", event);
+
+    return {
+        statusCode: 200,
+    };
+};
+
+exports.handler = schedule("@hourly", handler);
+
